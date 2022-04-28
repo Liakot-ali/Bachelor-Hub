e150b1460cc381ce0b4f570ab5b19627d7424b52
Md Nurnobi Hosen <mdnurnobihosen1@gmail.com>
2022-04-28 15:15:16 +0600

    Double tap Back Button added on Auth Acticity and onBoard Activity

--- app/src/main/java/com/nurnobishanto/bachelorhub/Activity/AuthActivity.java
+++ app/src/main/java/com/nurnobishanto/bachelorhub/Activity/AuthActivity.java
@@ -3,6 +3,9 @@ package com.nurnobishanto.bachelorhub.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import com.nurnobishanto.bachelorhub.AuthFragment.SigninFragment;
 import com.nurnobishanto.bachelorhub.R;
@@ -15,4 +18,30 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer,new SigninFragment()).commit();
     }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer,
+                new SigninFragment()).commit();
+        if (doubleBackToExitPressedOnce) {
+
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
 }
--- app/src/main/java/com/nurnobishanto/bachelorhub/Activity/onBoardAtivity.java
+++ app/src/main/java/com/nurnobishanto/bachelorhub/Activity/onBoardAtivity.java
@@ -8,13 +8,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nurnobishanto.bachelorhub.Adapter.ViewPagerAdapter;
+import com.nurnobishanto.bachelorhub.AuthFragment.SigninFragment;
 import com.nurnobishanto.bachelorhub.R;
 
 public class onBoardAtivity extends AppCompatActivity {
@@ -110,6 +114,30 @@ public class onBoardAtivity extends AppCompatActivity {
 
         }
     };
+
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+
+        if (doubleBackToExitPressedOnce) {
+
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 
 
 }
