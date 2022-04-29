ca9ffd48f406e822ceed1d34f92dcebc84c2f15e
Md Nurnobi Hosen <mdnurnobihosen1@gmail.com>
2022-04-29 18:23:53 +0600

    AuthActivity - Signup Fragment Design and Implement

--- app/src/main/java/com/nurnobishanto/bachelorhub/AuthFragment/SignUpFragment.java
+++ app/src/main/java/com/nurnobishanto/bachelorhub/AuthFragment/SignUpFragment.java
@@ -4,63 +4,235 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.nurnobishanto.bachelorhub.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
+
 public class SignUpFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignUpFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUpFragment newInstance(String param1, String param2) {
-        SignUpFragment fragment = new SignUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextInputLayout nameInputLayout,emailInputLayout,passInputLayout,confirmPassInputLayout,reflInputLayout;
+    private TextInputEditText nameInput,emailInput,passInput,confirmPassInput,refInput;
+    private Button signup;
+    private TextView signin,forget;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        nameInputLayout=view.findViewById(R.id.nameInputLayout);
+        emailInputLayout=view.findViewById(R.id.emailInputLayout);
+        passInputLayout=view.findViewById(R.id.passlInputLayout);
+        confirmPassInputLayout=view.findViewById(R.id.confirmPasslInputLayout);
+        reflInputLayout=view.findViewById(R.id.reflInputLayout);
+        nameInput=view.findViewById(R.id.name);
+        emailInput=view.findViewById(R.id.email);
+        passInput=view.findViewById(R.id.password);
+        confirmPassInput=view.findViewById(R.id.confirmPassword);
+        refInput=view.findViewById(R.id.refInput);
+        signin=view.findViewById(R.id.signin);
+        signup=view.findViewById(R.id.signup);
+        forget=view.findViewById(R.id.forget);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Validate()){
+
+                    Register();
+                }
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer,new SigninFragment()).commit();
+            }
+        });
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer,new ForgotFragment()).commit();
+            }
+        });
+        nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (nameInput.getText().toString().isEmpty()){
+                    nameInputLayout.setErrorEnabled(true);
+                    nameInputLayout.setError("Name is required!");
+                }else {
+                    nameInputLayout.setErrorEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        emailInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!emailInput.getText().toString().trim().matches(emailPattern)){
+                    emailInputLayout.setErrorEnabled(true);
+                    emailInputLayout.setError("Valid email is required!");
+                }else {
+                    emailInputLayout.setErrorEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        passInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (passInput.getText().toString().length()<8){
+                    passInputLayout.setErrorEnabled(true);
+                    passInputLayout.setError("Password must be at least 8 Character!");
+                }else {
+                    passInputLayout.setErrorEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        confirmPassInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (Objects.requireNonNull(passInput.getText()).toString().equals(confirmPassInput.getText().toString())){
+                    confirmPassInputLayout.setErrorEnabled(false);
+                }else {
+                    confirmPassInputLayout.setErrorEnabled(true);
+                    confirmPassInputLayout.setError("Confirm Password Not Match Yet!");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        refInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (refInput.getText().toString().length()<6){
+                    reflInputLayout.setErrorEnabled(false);
+                }else {
+                    reflInputLayout.setErrorEnabled(true);
+                    reflInputLayout.setError("Invalid Referral Code!");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return  view;
+    }
+
+    private void Register() {
+    }
+
+    private boolean Validate() {
+        if (nameInput.getText().toString().isEmpty()){
+            nameInputLayout.setErrorEnabled(true);
+            nameInputLayout.setError("Name is required!");
+            return false;
+        }
+        else if (emailInput.getText().toString().isEmpty()){
+            nameInputLayout.setErrorEnabled(false);
+            emailInputLayout.setErrorEnabled(true);
+            emailInputLayout.setError("Email is required!");
+            return false;
+        }else if(!emailInput.getText().toString().trim().matches(emailPattern)){
+            nameInputLayout.setErrorEnabled(false);
+            emailInputLayout.setErrorEnabled(true);
+            emailInputLayout.setError("Valid email is required!");
+            return false;
+        }
+        else if (passInput.getText().toString().isEmpty()){
+            emailInputLayout.setErrorEnabled(false);
+            passInputLayout.setErrorEnabled(true);
+            passInputLayout.setError("Password is required!");
+            return false;
+        }
+        else if (passInput.getText().toString().length()<8){
+            emailInputLayout.setErrorEnabled(false);
+            passInputLayout.setErrorEnabled(true);
+            passInputLayout.setError("Password must be at least 8 Character!");
+            return false;
+        }
+        else if (!Objects.requireNonNull(passInput.getText()).toString().equals(confirmPassInput.getText().toString())){
+            passInputLayout.setErrorEnabled(false);
+            confirmPassInputLayout.setErrorEnabled(true);
+            confirmPassInputLayout.setError("Confirm Password Not Match!");
+            return false;
+        }
+        else if (refInput.getText().toString().length()>5){
+            reflInputLayout.setErrorEnabled(true);
+            confirmPassInputLayout.setErrorEnabled(false);
+            reflInputLayout.setError("Invalid Referral Code!");
+            return false;
+        }
+        else {
+            confirmPassInputLayout.setErrorEnabled(false);
+            passInputLayout.setErrorEnabled(false);
+            nameInputLayout.setErrorEnabled(false);
+            emailInputLayout.setErrorEnabled(false);
+            reflInputLayout.setErrorEnabled(false);
+            return true;
+        }
+
     }
 }
