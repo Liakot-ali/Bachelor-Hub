203ce95220d8338cac80e39668d0f5a4bbc1f891
Liakot-ali <liakotali2k18@gmail.com>
2022-08-13 23:17:51 +0600

    removing redundant code

--- app/src/main/java/com/nurnobishanto/bachelorhub/MainActivity.java
+++ app/src/main/java/com/nurnobishanto/bachelorhub/MainActivity.java
@@ -567,7 +567,6 @@ public class MainActivity extends AppCompatActivity {
         getUserInformation();
     }
 
-    boolean doubleBackToExitPressedOnce = false;
     long currentTime = System.currentTimeMillis();
 
     @Override
@@ -583,20 +582,6 @@ public class MainActivity extends AppCompatActivity {
             currentTime = System.currentTimeMillis();
             Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show();
         }
-//        if (doubleBackToExitPressedOnce) {
-//            super.onBackPressed();
-//            return;
-//        }
-//        this.doubleBackToExitPressedOnce = true;
-//        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
-//
-//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                doubleBackToExitPressedOnce = false;
-//            }
-//        }, 2000);
     }
 
 
