cdab90e1c55f6e51525a130ec7a25f6879800f84
Liakot-Ali <liakotali2k18@gmail.com>
2022-08-30 21:53:48 +0600

    Creating verification details activity

---
+++ app/src/main/java/com/bachelorhub/bytecode/Admin/VerificationDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.bachelorhub.bytecode.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bachelorhub.bytecode.R;
+
+public class VerificationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vefication_details);
+    }
+}
